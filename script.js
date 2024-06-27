cb038ad13285b882a61f2cfd12753b7ee98bd0ef
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-27 10:24:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function() {
   function setCookie(name, value, days) {
     const expires = new Date();
     expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
-    document.cookie = `${name}=${value};expires=${expires.toUTCString()}`;
+    document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
   }
 
   
